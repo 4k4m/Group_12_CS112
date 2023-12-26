f3ad08ea0d94b498f9b3847068157f8f1240f4fa
Phuc Le <121591589+4k4m@users.noreply.github.com>
2023-12-27 00:57:57 +0700

    Insert comment to problem A

--- Week 15/probA.py
+++ Week 15/probA.py
@@ -1,4 +1,8 @@
 n, m = map(int, input().split())
+'''
+sumL[i]: satisfactions from 1 to i 
+sumR[i]:  satisfaction from i to m
+'''
 sumL, sumR = [0] * (m + 1) , [0] * (m + 1)
 result = 0
 
@@ -17,9 +21,8 @@ for i in range(m - 1, 0, -1):
     sumR[i] += sumR[i + 1]
 
 result = 0
+#leave the i-th dish, then the ans will be sumL[i - 1] + sumR[i + 1]
 for i in range(1, m):
     result = max(result, sumL[i - 1] + sumR[i + 1])
 
 print(result)
-
-    
